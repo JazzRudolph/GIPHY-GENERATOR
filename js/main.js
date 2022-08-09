318b560b899a7fca5ece27a9a896b38335d09364
Jazz Rudolph <jazzrudolph92@outlook.com>
2022-08-08 22:13:23 -0400

    Figured out how to get the search results to clear after every new search so the gifs are not stacked on top of each other.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 //My first API
 let APIKEY = "PXF0UFvNKUitBnxN3KFQ6PRN01bRq1to";
-      document.addEventListener("DOMContentLoaded", init);
-      function init() {
-        document.getElementById("btnSearch").addEventListener("click", ev => {
-          ev.preventDefault(); //to stop the page reload
-          let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-          let str = document.getElementById("search").value.trim();
-          url = url.concat(str);
-          console.log(url);
-          fetch(url)
-            .then(response => response.json())
-            .then(content => {
-              console.log(content.data);
-              console.log("META", content.meta);
-              let fig = document.createElement("figure");
-              let img = document.createElement("img");
-              img.src = content.data[0].images.downsized.url;
-              img.alt = content.data[0].title;
-              fig.appendChild(img);
-              let out = document.querySelector(".out");
-              out.insertAdjacentElement("afterbegin", fig);
-              document.querySelector("#search").value = "";
-              
-            })
-            .catch(err => {
-              console.error(err);
-            });
-        });
-      }
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+  document.getElementById("btnSearch").addEventListener("click", (ev) => {
+    ev.preventDefault(); //to stop the page reload
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+    let str = document.getElementById("search").value.trim();
+    url = url.concat(str);
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((content) => {
+        console.log(content.data);
+        console.log("META", content.meta);
+        let fig = document.createElement("figure");
+        let img = document.createElement("img");
+        img.src = content.data[0].images.downsized.url;
+        img.alt = content.data[0].title;
+        fig.appendChild(img);
+        let out = document.querySelector(".out");
+        out.innerHTML = "";
+        out.insertAdjacentElement("afterbegin", fig);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+}
